146e44130de6b3c6a68f7b4ad38db610fc198ba3
WyattChase <esnaruto@yahoo.com>
2023-09-22 05:38:33 +0000

    add navbar and basic front page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Navbar} from './Components/Navbar';
+
+
 
 function App() {
   return (
+    <><Navbar />
     <div className="App">
-      <h1>this works!</h1>
-    </div>
+      <div className="container">
+      <h1 className='d-flex justify-content-center mt-2 mb-2'>Music Playlist!</h1>
+      <div className="d-flex justify-content-center">
+      <input type="text" placeholder="Search"></input>
+      </div>
+      
+      </div>
+    </div></>
   );
 }
 
